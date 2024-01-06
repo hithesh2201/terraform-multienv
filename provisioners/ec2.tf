58455a62b96a5033fcb97440eed337fa264beecd
Hitesh <hithesh2201@gmail.com>
2024-01-06 13:16:16 +0530

    multi-env

---
+++ provisioners/ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "ec2" {
+    ami=var.ami_id
+    instance_type="t2.micro"
+    vpc_security_group_ids= ["sg-0f0225d73ad9227cc"]
+    tags = {
+      Name= "provisioner"
+    }
+    provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> private_ips.txt"
+  }
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+     "sudo yum install nginx -y",
+     "sudo systemctl start nginx"
+    ]
+  }
+}
---
+++ provisioners/variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {
+  type=string
+  default="ami-03265a0778a880afb"
+  
+}
+
+
